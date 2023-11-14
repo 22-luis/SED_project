66b1c5934ca1c13f77aa8b39fb5d8bcdbaad7ed2
22-luis <00016720@uca.edu.sv>
2023-11-13 22:44:02 -0600

    backend basic conf

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = process.env.port ?? 3000
+
+app.disable('x-powered-by')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello' })
+});
+
+app.get('/report', (req, res) => {
+    res.json({ message: 'Here the reports...maybe' })
+});
+
+app.post('/forms', (req, res) => {
+    //save in database
+    res.status(201).json(req.body)
+});
+
+app.use((req, res) => {
+    res.status(404).send('<h1>404 Not Found</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`The app listening on port http://localhost:${port}`)
+})
